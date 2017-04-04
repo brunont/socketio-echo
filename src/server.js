6b42e22e8f4dad0b016a8c44fa73e9d5bfbdf83e
Bruno Torquato <brunont@gmail.com>
2017-04-03 22:08:23 -0300

    Adding Echo

--- src/server.js
+++ src/server.js
@@ -1,21 +1,33 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
+const express = require('express')
+const socketIO = require('socket.io')
+const path = require('path')
 
-const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, '..', 'public', 'index.html');
+const PORT = process.env.PORT || 3000
+const TIME = path.join(__dirname, '..', 'public', 'time.html')
+const ECHO = path.join(__dirname, '..', 'public', 'echo.html')
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .get('/time', (req, res) => {
+    console.log('Rendering TIME')
+    res.sendFile(TIME)
+  })
+  .get('/', (req, res) => {
+    console.log('Rendering ECHO')
+    res.sendFile(ECHO)
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-const io = socketIO(server);
+const io = socketIO(server)
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
+  console.log('Client connected')
+  socket.on('disconnect', () => console.log('Client disconnected'))
+  socket.on('msg', (msg) => {
+    console.log('received: %s', msg)
+    socket.emit('msg', msg)
+  })
+})
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
