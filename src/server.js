e4975370f592be5d2be3692bfb72a415c22503fc
Bruno Torquato <brunont@gmail.com>
2017-04-05 18:32:55 -0300

    Disabling Time server

--- src/server.js
+++ src/server.js
@@ -63,4 +63,4 @@ io.on('connection', (socket) => {
   })
 })
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
